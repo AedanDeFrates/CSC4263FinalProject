d35e3f0815f682f2b1d0a5ef2960c7442de8d1e3
AedanDeFrates <adefra5@lsu.edu>
2026-04-22 23:21:52 -0500

    Added crouch. Added item range. Added Item interaction.

--- Indigo/Assets/Scripts/Player/PlayerMove.cs
+++ Indigo/Assets/Scripts/Player/PlayerMove.cs
@@ -3,30 +3,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
     public float speed = 5f;
+    public bool isMoving;
+
+    public Rigidbody2D rb;
     public InputActionReference move;
+    private PlayerCrouch playerCrouch;
     private Vector2 moveInput;
 
-
+    void Start()
+    {
+        playerCrouch = GetComponent<PlayerCrouch>();
+    }
     private void OnEnable()
     {
         move.action.Enable();
     }
-
     private void OnDisable()
     {
         move.action.Disable();
     }
-
-
     void Update()
     {
         moveInput = move.action.ReadValue<Vector2>();
     } 
-
     void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * speed;
+        if(playerCrouch.crouchInput.action.IsPressed())
+        {
+            rb.linearVelocity = moveInput * playerCrouch.crouchSpeed;
+        }
+        else { rb.linearVelocity = moveInput * speed; }
     }
 }
